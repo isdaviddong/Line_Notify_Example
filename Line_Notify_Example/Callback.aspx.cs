3010e4c2d57df7a8a6488e94ff19e3bb57e6259a
twdeveloper <twdeveloper@outlook.com>
2021-08-15 13:01:27 +0800

    upgrade packages, fix typo

--- Line_Notify_Example/Callback.aspx.cs
+++ Line_Notify_Example/Callback.aspx.cs
@@ -23,7 +23,7 @@ namespace Line_Notify_Example
             //顯示，測試用
             Response.Write("<br/> code : " + code);
             //從Code取回toke
-            var token = Utility.GetToeknFromCode(code,
+            var token = Utility.GetTokenFromCode(code,
                 "uf8x32KjCX假的UwsX83OXNI2",  //TODO:請更正為你自己的 client_id
                 "ZeQH6lX!Bv假的O0pq假的x7NYYK4假的XT$lvf7p假的2lyY", //TODO:請更正為你自己的 client_secret
                 "http://localhost:3696/Callback.aspx");
